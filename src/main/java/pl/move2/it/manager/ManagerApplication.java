d3353234c8956d3772182b1994ebcf425cd56a57
Boryszs <12damie@gmail.com>
2022-06-20 19:55:01 +0200

    Update change dependency add (groovy, spock), remove (spring cloud)

--- src/main/java/pl/move2/it/manager/ManagerApplication.java
+++ src/main/java/pl/move2/it/manager/ManagerApplication.java
@@ -2,11 +2,7 @@ package pl.move2.it.manager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@EnableEurekaServer // Eureka Server
-@EnableFeignClients // Feign Clients
 @SpringBootApplication
 public class ManagerApplication {
 
